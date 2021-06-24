114949519c72338ef4866529b320f7c431324d2e
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-24 10:35:11 -0400

    added student and goals posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,17 @@ class ApplicationController < Sinatra::Base
   get "/teachers" do
     Teacher.first.to_json(include: [{students: {include: {studentgoals: {methods: [:goal_title]}}}}, :goals]) 
   end
+
+  post '/students' do
+    new_student = Student.create(name: params[:name], grade: params[:grade], hair_color: params[:hair_color],
+       siblings_amount: params[:siblings_amount], teacher_id: params[:teacher_id])
+       
+    new_student.to_json
+  end
+
+  post '/goals' do
+    new_goal = Goal.create(title: params[:title], description: params[:description], teacher_id: params[:teacher_id])
+
+    new_goal.to_json
+  end
 end
